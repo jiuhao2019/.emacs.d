6b281edcc6cb3a7054edca20158963bb6ac34c64
jiuhao <815514981@qq.com>
2024-10-16 11:04:42 +0800

    update

--- init.el
+++ init.el
@@ -144,6 +144,7 @@
 (require 'user-lisp-format)
 (require 'user-valign);;能让表格里有中文的表格对齐,但是如打开的文件里表格多且大就卡顿。
 (require 'user-rg)
+(require 'user-pyim)
 (use-package amx ;;auto show recent commands
   :init (amx-mode))
 
