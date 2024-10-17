b78f8c5a77ed02d731cad30d8cdf3c4d604f9e6f
jiuhao <815514981@qq.com>
2024-10-17 23:10:32 +0800

    update

--- lisp/user-theme.el
+++ lisp/user-theme.el
@@ -9,24 +9,25 @@
   ((doom-modeline-height 15)))
 
 ;;doom themes
-(use-package doom-themes
-  :config
-  ;; Global settings (defaults)
-  (setq doom-themes-enable-bold nil    ; if nil, bold is universally disabled
-        doom-themes-enable-italic nil) ; if nil, italics is universally disabled
-  (load-theme 'doom-one t)
+;; (use-package doom-themes
+;;   :config
+;;   ;; Global settings (defaults)
+;;   (setq doom-themes-enable-bold nil    ; if nil, bold is universally disabled
+;;         doom-themes-enable-italic nil) ; if nil, italics is universally disabled
+;;   (load-theme 'doom-one t)
 
-  ;; Enable flashing mode-line on errors
-  (doom-themes-visual-bell-config)
-  ;; Enable custom neotree theme (all-the-icons must be installed!)
-  (doom-themes-neotree-config)
-  ;; Corrects (and improves) org-mode's native fontification.
-  (doom-themes-org-config))
+;;   ;; Enable flashing mode-line on errors
+;;   (doom-themes-visual-bell-config)
+;;   ;; Enable custom neotree theme (all-the-icons must be installed!)
+;;   (doom-themes-neotree-config)
+;;   ;; Corrects (and improves) org-mode's native fontification.
+;;   (doom-themes-org-config))
 
 ;;gruvbox theme
 ;; (use-package gruvbox-theme
 ;;   :config
 ;;   (load-theme 'gruvbox-dark-hard t))
 
+(load-theme 'wombat t)
 ;;
 (provide 'user-theme)
