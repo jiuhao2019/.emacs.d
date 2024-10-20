cf8defa1ddb3573aebc8c9442dbc25d8601651f0
jiuhao <815514981@qq.com>
2024-10-20 16:01:46 +0800

    update

--- lisp/user-valign.el
+++ lisp/user-valign.el
@@ -3,7 +3,7 @@
   (setq valign-max-table-size 7000)
   (setq valign-fancy-bar t))
 ;;用的时候再使能
-;; (add-hook 'org-mode-hook #'valign-mode)
+(add-hook 'org-mode-hook #'valign-mode)
 
 (provide 'user-valign)
 
